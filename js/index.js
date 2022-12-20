8f73d683e6cc93140256d55af513fc7206755c95
Henrique Alves de Lima <henriquetolis@outlook.com>
2022-12-20 14:33:19 -0300

    Responsividade 2.1
    Feito os botões com javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
+var btn = document.querySelector('.show-or-hide');
+var btnOne = document.querySelector('.show-or-hide-one');
+var btnTwo = document.querySelector('.show-or-hide-two');
+
+
+var hide = document.querySelector("#classificar");
+var hideOne = document.querySelector("#cards");
+var hideTwo = document.querySelector("#bloco");
+
+
+
+btn.addEventListener('click', function() {
+
+    if(hide.style.display === 'flex') {
+        hide.style.display = 'none';
+    } else {
+        hide.style.display = 'flex';
+    }
+});
+btnOne.addEventListener('click', function() {
+
+    if(hideOne.style.display === 'flex') {
+        hideOne.style.display = 'none';
+    } else {
+        hideOne.style.display = 'flex';
+    }
+});
+btnTwo.addEventListener('click', function() {
+
+    if(hideTwo.style.display === 'inline-block') {
+        hideTwo.style.display = 'none';
+    } else {
+        hideTwo.style.display = 'inline-block';
+    }
+});
+
 function createGame(time, player1, score, player2) {
     return `
     <li class="time">
@@ -9,7 +45,6 @@ function createGame(time, player1, score, player2) {
         <img src="./assets/icon-${player2}.svg" alt="${player2}">
     </li><hr>   `
 }
-
 let delay = -0.3;
 function createCard(date, day, games){
     delay = delay + 0.3
@@ -22,8 +57,7 @@ function createCard(date, day, games){
      </div>   `
 }
 
-document.querySelector("#cards").innerHTML = 
-createCard("20/11", "Domingo", createGame("Grupo A", "catar", "0 x 2", "equador")) +
+hideOne.innerHTML = createCard("20/11", "Domingo", createGame("Grupo A", "catar", "0 x 2", "equador")) +
 createCard("21/11", "Segunda", createGame("Grupo A", "senegal", "0 x 2", "holanda") + createGame("Grupo B", "inglaterra", "6 x 2", "iran") + createGame(" ", "estadosUnidos", "1 x 1", "gales")) +
 createCard("22/11", "Terça", createGame("Grupo C", "argentina", "1 x 2", "arabiaSaudita") + createGame("", "mexico", "0 x 0", "polonia") + createGame("Grupo D", "dinamarca", "0 x 0", "tunisia") + createGame("", "franca", "4 x 1", "australia")) +
 createCard("23/11", "Quarta", createGame("Grupo E", "alemanha", "1 x 2", "japao") + createGame("", "espanha", "7 x 0", "costaRica") + createGame("Grupo F", "marrocos", "0 x 0", "croacia") + createGame("", "belgica", "1 x 0", "canada")) +
@@ -48,26 +82,28 @@ let newDelay = -0.3;
 function createClassificar(grupo, point){
     newDelay = newDelay + 0.3
     return `
-     <div class="card" style="animation-delay: ${newDelay}">
-     
+    
+     <div class="card"  style="animation-delay: ${newDelay}">
      <table class="classificarPontos">
         <tr><h2> Grupo ${grupo}</h2></tr>
         <thead><tr><td id="distancia">Equipe</td><td>Pontos</td><td>Jogos</td><td>Vitória</td><td>Empate</td><td>Derrota</td></tr></thead>
         ${point}
-        
      </table>
-        <hr>
+     <blockquote cite="https://www.fifa.com/fifaplus/pt/articles/copa-do-mundo-2022-selecoes-grupos-datas-ingressos" > Apenas os dois primeiros colocados por grupo é classificado.</blockquote>
+     <hr>
      </div>   `
 }
 
-document.querySelector("#classificar").innerHTML = createClassificar("A", createPoint("holanda","Holanda", "7", "2", "1", "0") + createPoint("senegal","Senegal", "6", "2", "0", "1") + createPoint("equador","Equador", "4", "1", "1", "1") + createPoint("catar","Catar", "0", "0", "0", "3")) + 
+hide.innerHTML = createClassificar("A", createPoint("holanda","Holanda", "7", "2", "1", "0") + createPoint("senegal","Senegal", "6", "2", "0", "1") + createPoint("equador","Equador", "4", "1", "1", "1") + createPoint("catar","Catar", "0", "0", "0", "3")) + 
 createClassificar("B", createPoint("inglaterra","Inglaterra", "7", "2", "1", "0") + createPoint("estadosUnidos","Estados Unidos", "5", "1", "2", "0") + createPoint("iran","Irã", "3", "1", "0", "2") + createPoint("gales","Gales", "1", "0", "1", "2")) + 
 createClassificar("C", createPoint("argentina","Argentina", "6", "2", "0", "1") + createPoint("polonia","Polônia", "4", "1", "1", "1") + createPoint("mexico", "México", "4", "1", "1", "1") + createPoint("arabiaSaudita","Arábia Saudita", "3", "1", "0", "2")) + 
 createClassificar("D", createPoint("franca","França", "6", "2", "0", "1") + createPoint("australia","Austrália", "6", "2", "0", "1") + createPoint("tunisia", "Tunísia", "4", "1", "1", "1") + createPoint("dinamarca","Dinamarca", "1", "0", "1", "2")) + 
 createClassificar("E", createPoint("japao","Japão", "6", "2", "0", "1") + createPoint("espanha","Espanha", "4", "1", "1", "1") + createPoint("alemanha", "Alemanha", "4", "1", "1", "1") + createPoint("costaRica","Costa Rica", "3", "1", "0", "2")) +
 createClassificar("F", createPoint("marrocos","Marrocos", "7", "1", "0", "1") + createPoint("croacia","Croácia", "5", "1", "2", "0") + createPoint("belgica", "Bélgica", "4", "1", "1", "1") + createPoint("canada","Canadá", "0", "0", "0", "3")) +
 createClassificar("G", createPoint("brazil","Brasil", "6", "2", "0", "1") + createPoint("suica","Suiça", "6", "2", "0", "1") + createPoint("camaroes", "Camarões", "4", "1", "1", "1") + createPoint("servia","Sérvia", "1", "0", "1", "2")) +  
-createClassificar("H", createPoint("portugal","Portugal", "6", "2", "0", "1") + createPoint("coreiaDoSul","Coreia Do Sul", "4", "1", "1", "1") + createPoint("uruguai", "Uruguai", "4", "1", "1", "1") + createPoint("gana","Gana", "3", "1", "0", "2"))
+createClassificar("H", createPoint("portugal","Portugal", "6", "2", "0", "1") + createPoint("coreiaDoSul","Coreia Do Sul", "4", "1", "1", "1") + createPoint("uruguai", "Uruguai", "4", "1", "1", "1") + createPoint("gana","Gana", "3", "1", "0", "2"));
+
+
 
 
 function createoitavas(player, namePlayer, pontos) {
@@ -113,7 +149,10 @@ createSeassonLast ("", createoitavas("franca", "França", "2 ") + createoitavas(
 
 document.querySelector("#final").innerHTML = createSeassonLast ("Final", createoitavas("argentina", "Argentina", "3 (4)") + createoitavas("franca", "França", "3 (2)"))
 
-document.querySelector("#terceiro").innerHTML = createSeassonLast ("Terceiro lugar", createoitavas("croacia", "Croácia", "2") + createoitavas("marrocos", "Marrocos", "1")) 
+document.querySelector("#terceiro").innerHTML = createSeassonLast ("Terceiro lugar", createoitavas("croacia", "Croácia", "2") + createoitavas("marrocos", "Marrocos", "1"));
+
+
+
 
 
 
